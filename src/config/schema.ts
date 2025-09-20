8266bab656792858835bfb18105bfeb9de140da3
AnnBalias <annbalias@gmail.com>
2025-09-20 13:08:51 +0300

    docker-with-mongodb

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -5,8 +5,15 @@ export const EnvSchema = {
   properties: {
     PORT: { type: 'number' },
     HOST: { type: 'string' },
+    
+    MONGO_URI: { type: 'string' },
+    MONGO_DATABASE: { type: 'string', default: 'newspoint' },
+    MONGO_ROOT_USERNAME: { type: 'string', default: 'admin' },
+    MONGO_ROOT_PASSWORD: { type: 'string', default: 'password' },
+    MONGO_EXPRESS_USERNAME: { type: 'string', default: 'admin' },
+    MONGO_EXPRESS_PASSWORD: { type: 'string', default: 'pass' },
   },
-  required: ['PORT', 'HOST'],
+  required: ['PORT', 'HOST', 'MONGO_URI'],
   additionalProperties: false,
 } as const;
 
