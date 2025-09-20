b2bd720b91af8baaeb5d2637aa477e18df1aa3de
AnnBalias <annbalias@gmail.com>
2025-09-20 16:37:29 +0300

    fix-linting

--- src/modules/feedParser/routes/feedParser.route.ts
+++ src/modules/feedParser/routes/feedParser.route.ts
@@ -32,7 +32,7 @@ export async function getFeedDataRoutes(fastify: FastifyInstance) {
               )
             );
         }
-        
+
         const feedData = {
           feeds: [],
           total: 0,
